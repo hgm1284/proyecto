b433b82249308f4f7ec9cf6cb5e2faeb64a8cdf5
hgm1284 <hgm1284@gmail.com>
2019-12-07 15:20:25 -0600

    seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+       $this->call(PeriodosTableSeeder::class);
+       $this->call(PrivilegiosTableSeeder::class);
+       $this->call(UsersTableSeeder::class);
     }
 }
