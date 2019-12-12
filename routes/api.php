4803cde3b5584b4b7742a7a4518a682bb0a1cf65
hgm1284 <hgm1284@gmail.com>
2019-12-12 00:08:42 -0600

    edit rol anual

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,17 @@ use Illuminate\Http\Request;
 |
 */
 
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
 
 Route::get('/enfermeras/{id}/datos', 'EnfermerasController@datosEnfermero');
 Route::get('/vacaciones/{id}/info', 'VacacionesController@calcularDiasVacaciones');
